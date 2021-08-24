d0e558007bf240d0cc2a43253f2497b642a47159
Kim Carlo Cortez <kuyakimpoy010@gmail.com>
2021-08-24 11:54:50 +0800

    created API for getting all employees in DB

--- routes/employees.js
+++ routes/employees.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const Employee = require('../models/Employee');
 
-
+router.get('/',async (req,res)=>{
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+})
 
 module.exports = router;
