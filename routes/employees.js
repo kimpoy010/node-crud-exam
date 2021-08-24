5b60d58c82aecc1dda3d752e1cda3cf90ec6e899
Kim Carlo Cortez <kuyakimpoy010@gmail.com>
2021-08-24 11:56:12 +0800

    created API for saving data to DB and retrieving single data

--- routes/employees.js
+++ routes/employees.js
@@ -8,4 +8,15 @@ router.get('/',async (req,res)=>{
     res.status(200).json(employees);
 })
 
+router.post('/add', async (req,res)=>{
+    const employee = new Employee(req.body);
+    const saveEmployee = await employee.save();
+    res.status(200).json(saveEmployee);
+});
+
+router.get('/single/:id', async (req,res)=>{
+    const e = await Employee.findById({ _id: req.params.id });
+	res.status(200).json(e);
+});
+
 module.exports = router;
